6ab6c02eef1a602d9cb94847e1d59c108f014f83
Jaime <jgomezm@tecsup.edu.pe>
2025-03-18 07:47:54 -0500

    update

--- Contents.swift
+++ Contents.swift
@@ -4,4 +4,26 @@ let hola:String = "Hola Mundo ...!"
 let mensaje = "Nuevo mensaje"
 let regalo:String = "3"
 let a:Double = 10.0
+let b:Double = 2.5
+let c:Int = 10
+let d:Int = 2
+let e:Character = "X"
+let f:Bool = true
+let suna = a + b
+let resta = c - d
+let division = a / c
+var multiplicacion = b * c
+let validaSuma = e == "X" ? suma : resta
+multiplicacion = 12.5
+let saludo = hola + hola2 + " su nota es  \(suna)"
+print(saludo)
+let extras = saludo + "  + bono de " + regalo + " e " + ( suna + regalo)
+print(extras)
+
+
+
+
+
+
+
 
